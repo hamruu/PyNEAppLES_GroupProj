4c3b83b769465a523cca72f2de2d6e41bee26a22
Felix Mac-Fall <felixmac-fall@eduroam-212-213.nomadic.bris.ac.uk>
2025-02-14 16:38:46 +0000

    Added function to represample for direct reading, tested this in module_call.py using code to randomly generate data. Geomreduction produced output'

--- Test_Module/src/Module_call.py
+++ Test_Module/src/Module_call.py
@@ -7,8 +7,20 @@
 #     "scipy",
 # ]
 # ///
+from tabnanny import verbose
+
+import numpy as np
 
 import test_module.repre_sample_2D as rstd
 
-print(help(rstd.read_cmd()))
+MYGEOM = rstd.GeomReduction(500, 3, 10, 10, 1, 1, weighted=False, pdfcomp = "KLdiv", intweights=0, verbose=False)
 
+# Set the seed for reproducibility
+np.random.seed(42)
+# Generate random float values around 2 with some noise
+excit_e = 2 + (np.random.rand(3, 500) - 0.5) * 4
+excit_e = np.clip(excit_e, 0, 4)
+trans_dip_mom = 10 + np.random.rand(3, 500, 3) * 90
+
+MYGEOM.read_data_direct(excit_e, trans_dip_mom)
+MYGEOM.reduce_geoms()
