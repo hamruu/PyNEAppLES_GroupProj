0768006451c7dc63bc5ee226078c3bd15b0031ce
Fel1-x <150053138+Fel1-x@users.noreply.github.com>
2025-02-15 16:01:22 +0000

    removed generated files, and adjusted the package to function correctly with TDM x,y,z inputs

--- Test_Module/src/Module_call.py
+++ Test_Module/src/Module_call.py
@@ -7,21 +7,23 @@
 #     "scipy",
 # ]
 # ///
-
 from tabnanny import verbose
 
 import numpy as np
 
 import test_module.repre_sample_2D as rstd
 
-MYGEOM = rstd.GeomReduction(500, 3, 10, 10, 1, 1, weighted=False, pdfcomp = "KLdiv", intweights=0, verbose=False)
+MYGEOM = rstd.GeomReduction(500, 3, 20, 100, 1, 1, weighted=False, pdfcomp = "KLdiv", intweights=0, verbose=False)
 
 # Set the seed for reproducibility
 np.random.seed(42)
 # Generate random float values around 2 with some noise
-excit_e = 2 + (np.random.rand(3, 500) - 0.5) * 4
+excit_e = 2 + (np.random.rand(500, 3) - 0.5) * 4
 excit_e = np.clip(excit_e, 0, 4)
-trans_dip_mom = 10 + np.random.rand(3, 500, 3) * 90
 
-MYGEOM.read_data_direct(excit_e, trans_dip_mom)
+trans_dip_mom_x = 10 + np.random.rand(500, 3) * 90
+trans_dip_mom_y = 10 + np.random.rand(500, 3) * 90
+trans_dip_mom_z = 10 + np.random.rand(500, 3) * 90
+
+MYGEOM.read_data_direct(excit_e, trans_dip_mom_x, trans_dip_mom_y, trans_dip_mom_z)
 MYGEOM.reduce_geoms()
